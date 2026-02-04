852168a217d5bf0fd299e6f49f87539db012312f
lankut <lankute@mail.ru>
2026-02-04 09:04:31 +0300

    try 2

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -31,10 +31,6 @@ export const App = () => {
     setIsInitialized(true)
   }, [isLoading])
 
-  useEffect(() => {
-    console.log("?")
-  }, [])
-
   if (!isInitialized) {
     return (
       <div className={styles.circularProgressContainer}>
