c361468bc7144677979bd3e6a88cbe24b4ee6a0c
lankut <lankute@mail.ru>
2026-02-04 09:22:43 +0300

    &

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -31,6 +31,10 @@ export const App = () => {
     setIsInitialized(true)
   }, [isLoading])
 
+  useEffect(() => {
+
+  }, [])
+
   if (!isInitialized) {
     return (
       <div className={styles.circularProgressContainer}>
